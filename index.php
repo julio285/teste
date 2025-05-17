0e4498521694181c60e589bbe992ffe67fe148f7
julio285 <julioaeronet@gmail.com>
2025-05-17 17:51:32 -0300

    update

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    /*$nota = 4;
+    if ($nota >= 5){
+        echo 'Oaluno foi aprovado';
+}else if ($nota < 5) {
+        echo 'O aluno esta de recuperacão';
+    } else {
+        echo 'O anulo foi reprovado';
+    }*/
+    function aguarde() {
+        echo 'Carregando site...';
+    }
+    aguarde();
+    echo'<br>';
+
+    ?>
+</body>
+</html>
